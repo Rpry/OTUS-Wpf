54d12490422719a92f247edafdd0f5b7cb9dabfe
rprikhodko <rprikhodko@ozon.ru>
2026-04-10 18:02:05 +0300

    Initial

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e) 
+        {
+            await Task.Delay(1000).ConfigureAwait(false);
+            textbox1.Text = "value";
+        }
+    }
+}
